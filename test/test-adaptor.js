916a19f3c4aeec2d414f38a2d43ee8f5e882a964
Thomas Peri <tjperi@gmail.com>
2023-08-14 14:57:21 -0700

    Test without mojl itself

--- test/test-adaptor.js
+++ test/test-adaptor.js
@@ -1,31 +1,28 @@
 /*global describe, it */
 const assert = require('assert');
-const fs = require('fs'); // eslint-disable-line no-unused-vars
 const path = require('path').posix; // eslint-disable-line no-unused-vars
-const DirectoryTester = require('../DirectoryTester.js');
-const { name, cloneRun } = new DirectoryTester(__filename);
 
-const Mojl = require('mojl');
-const cssMinifierAdaptor = path.join(__dirname, '../index.js');
+const cssMinifierAdapter = require('../index.js');
 
-describe(name, async () => {
-
+describe('mojl-clean-css tests', async () => {
 	it('should minify stylesheets', async () => {
-		await cloneRun(async (base, box) => { // eslint-disable-line no-unused-vars
-			const mojl = new Mojl({
-				cssMinifierAdaptor
-			});
-			await mojl.build();
-			
-			const after = box.snapshot();
-			
-			assert.equal(
-				after['dist/site.css'],
-				'.bar{color:#faebd7}.foo{color:#40e0d0}'
-			);
-			
-		});
-	});
+		
+		let source = `
+			/* a stylesheet for bar */
+			.bar {
+				color: antiquewhite;
+			}
 
+			/* a stylesheet for foo */
+			.foo {
+				color: turquoise;
+			}
+		`;
+		
+		let actual = await cssMinifierAdapter(source);
+		let expected = '.bar{color:#faebd7}.foo{color:#40e0d0}';
+		assert.equal(actual, expected);
+
+	});
 });
 
